07f6e87def58c5dec5501da968f1decf64d479a8
Zain Ul Abdin <152189719+uncle-joshi@users.noreply.github.com>
2026-06-29 19:38:03 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -86,14 +86,78 @@ void check_password_strength() {
             continue; 
         }
         
-        bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+        int upperCount = 0, lowerCount = 0, digitCount = 0, specialCount = 0;
+        int score = 0;
         
+        // Count each type of character
         for (char c : password) {
-            if (uppercase_letters.find(c) != string::npos) hasUpper = true;
-            else if (lowercase_letters.find(c) != string::npos) hasLower = true;
-            else if (digits.find(c) != string::npos) hasDigit = true;
-            else if (special_characters.find(c) != string::npos) hasSpecial = true;
+            if (uppercase_letters.find(c) != string::npos) {
+                upperCount++;
+            } 
+            else if (lowercase_letters.find(c) != string::npos) {
+                lowerCount++;
+            } 
+            else if (digits.find(c) != string::npos) {
+                digitCount++;
+            } 
+            else if (special_characters.find(c) != string::npos) {
+                specialCount++;
+            } 
         }
+        
+        // Score Uppercase: 1-2 = +1, 3-5 = +2, 6+ = +3
+        if (upperCount >= 1 && upperCount <= 2) {
+            score += 1;
+        } else if (upperCount >= 3 && upperCount <= 5) {
+            score += 2;
+        } else if (upperCount >= 6) {
+            score += 3;
+        }
+        
+        // Score Lowercase: 1-2 = +1, 3-5 = +2, 6+ = +3
+        if (lowerCount >= 1 && lowerCount <= 2) {
+            score += 1;
+        } else if (lowerCount >= 3 && lowerCount <= 5) {
+            score += 2;
+        } else if (lowerCount >= 6) {
+            score += 3;
+        }
+        
+        // Score Digits: 1-2 = +1, 3-5 = +2, 6+ = +3
+        if (digitCount >= 1 && digitCount <= 2) {
+            score += 1;
+        } else if (digitCount >= 3 && digitCount <= 5) {
+            score += 2;
+        } else if (digitCount >= 6) {
+            score += 3;
+        }
+        
+        // Score Special: 1-2 = +2, 3-5 = +4, 6+ = +6
+        if (specialCount >= 1 && specialCount <= 2) {
+            score += 2;
+        } else if (specialCount >= 3 && specialCount <= 5) {
+            score += 4;
+        } else if (specialCount >= 6) {
+            score += 6;
+        }
+        
+        // Length bonus
+        if (password.length() >= 8 && password.length() <= 10) {
+            score += 1;
+        } else if (password.length() >= 11 && password.length() <= 13) {
+            score += 2;
+        } else if (password.length() >= 14 && password.length() <= 16) {
+            score += 3;
+        }
+        
+        // Cap score at 10
+        if (score > 10) score = 10;
+        
+        // Check if all requirements are met
+        bool hasUpper = upperCount > 0;
+        bool hasLower = lowerCount > 0;
+        bool hasDigit = digitCount > 0;
+        bool hasSpecial = specialCount > 0;
         
         if (!hasUpper || !hasLower || !hasDigit || !hasSpecial) {
             cout << red << "WEAK PASSWORD! Must contain:" << reset << endl;
@@ -103,8 +167,54 @@ void check_password_strength() {
             if (!hasSpecial) cout << yellow << "- At least one special character" << reset << endl;
             continue;
         }
+
+        cout << "\nPassword analysis:" << endl;
+        cout << "  Uppercase: " << upperCount << " (Score: ";
+        if (upperCount >= 1 && upperCount <= 2) cout << "+1";
+        else if (upperCount >= 3 && upperCount <= 5) cout << "+2";
+        else if (upperCount >= 6) cout << "+3";
+        else cout << "+0";
+        cout << ")" << endl;
         
-        cout << green << "STRONG PASSWORD! All requirements met.\n" << reset;
+        cout << "  Lowercase: " << lowerCount << " (Score: ";
+        if (lowerCount >= 1 && lowerCount <= 2) cout << "+1";
+        else if (lowerCount >= 3 && lowerCount <= 5) cout << "+2";
+        else if (lowerCount >= 6) cout << "+3";
+        else cout << "+0";
+        cout << ")" << endl;
+        
+        cout << "  Digits: " << digitCount << " (Score: ";
+        if (digitCount >= 1 && digitCount <= 2) cout << "+1";
+        else if (digitCount >= 3 && digitCount <= 5) cout << "+2";
+        else if (digitCount >= 6) cout << "+3";
+        else cout << "+0";
+        cout << ")" << endl;
+        
+        cout << "  Special: " << specialCount << " (Score: ";
+        if (specialCount >= 1 && specialCount <= 2) cout << "+2";
+        else if (specialCount >= 3 && specialCount <= 5) cout << "+4";
+        else if (specialCount >= 6) cout << "+6";
+        else cout << "+0";
+        cout << ")" << endl;
+        
+        cout << "  Length: " << password.length() << " (Bonus: ";
+        if (password.length() >= 8 && password.length() <= 10) cout << "+1";
+        else if (password.length() >= 11 && password.length() <= 13) cout << "+2";
+        else if (password.length() >= 14 && password.length() <= 16) cout << "+3";
+        cout << ")" << endl;
+        
+        cout << "\n" << green << "Total Score: " << score << " out of 10" << reset << endl;
+        
+        // Rating based on score
+        if (score >= 9) {
+            cout << green << "EXCELLENT PASSWORD! Very strong!" << reset << endl;
+        } else if (score >= 7) {
+            cout << green << "STRONG PASSWORD! All requirements met." << reset << endl;
+        } else if (score >= 5) {
+            cout << yellow << "MODERATE PASSWORD - Consider adding more variety." << reset << endl;
+        } else {
+            cout << red << "WEAK PASSWORD - Improve with more character types." << reset << endl;
+        }
         break;  
     }
 }
